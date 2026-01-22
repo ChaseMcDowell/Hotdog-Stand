2ea4a560b8a1f9be94dbe191008a38bb805f9599
ChaseMcDowell <cm545347@georgetownisd.org>
2026-01-22 16:04:42 -0600

    shop.py

--- shop.py
+++ shop.py
@@ -13,9 +13,43 @@ def shop():
     global buns
     global ketchup
 
-    shopChoice = int(input("Purchase: \n 1) meat $1 \n 2) buns $.50 \n 3) ketchup $.25"))
+    shopChoice = int(input("Purchase: \n 1) meat $1 \n 2) buns $.50 \n 3) ketchup $.25 \n 4) Exit shop"))
     shopAmount = int(input("How many would you like to buy? "))
+    #if they chose meat
     if shopChoice == 1:
         money -= shopAmount
         if money < 0:
             money += shopAmount
+            print("You cannot afford this!")
+            shop()
+        else:
+            meat += shopAmount
+            shop()
+    #if they choose buns
+    if shopChoice == 2:
+        money -= 0.5*shopAmount
+        if money < 0:
+            money += 0.5*shopAmount
+            print("You cannot afford this!")
+            shop()
+        else:
+            buns += shopAmount
+            shop()
+    #if they choose ketchup
+    if shopChoice == 3:
+        money -= 0.25*shopAmount
+        if money < 0:
+            money += 0.25*shopAmount
+            print("You cannot afford this!")
+            shop()
+        else:
+            ketchup += shopAmount
+            shop()        
+    #if they want to leave
+    if shopChoice == 4:
+        None
+    else:
+        print("Please enter a valid number")
+        shop()
+  
+shop()
