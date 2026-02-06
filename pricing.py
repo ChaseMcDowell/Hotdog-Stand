51482fe22b8ec22d8f6a0f374309ea014f390bd1
ChaseMcDowell <cm545347@georgetownisd.org>
2026-02-06 10:22:37 -0600

    sdf

--- pricing.py
+++ pricing.py
@@ -6,11 +6,18 @@ def price(ingredients,inventory):
     while True:
         global pricePer
         costPer = ingredients["meat"] + 0.50 + ingredients["ketchup"]*0.25 + ingredients["mustard"]*0.25
-        produce = {"numofmeat" : inventory["meat"] / ingredients["meat"],
-                   "numofbuns" : inventory["buns"],
-                   "numofketchup" :inventory["ketchup"] / ingredients["ketchup"],
-                   "numofmustard" : inventory["mustard"] / ingredients["mustard"]}
-        numOfMake = min(produce.values())
+        produce = inventory
+        numOfMake = 0
+        
+        for i in range(produce["buns"]):
+            produce["meat"] -= 1
+            produce["ketchup"] -= 1
+            produce["mustard"] -= 1
+            if produce["meat"] < 1 or produce["ketchup"] < 1 or produce["mustard"] < 1:
+                break
+            else:
+                numOfMake += 1
+
         print(numOfMake)
         print(f"Cost per hotdog: {costPer}")
         print(f"Price per hotdog: {pricePer}")
