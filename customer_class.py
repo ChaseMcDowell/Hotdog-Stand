0021511165de67142c32c1465aa41c7f5cf07ba7
DavisLBradley13 <db536877@georgetownisd.org>
2026-02-09 15:23:17 -0600

    Start of Bench 4
    Via Customer Class, Recipe, Startday

--- customer_class.py
+++ customer_class.py
@@ -8,23 +8,27 @@
 import random
 
 class Customer:
-    def __init__(self, sweetness_preference=None, ice_preference=None, price_point=None):
-        self.sweetness_preference = (
-            sweetness_preference if sweetness_preference is not None
+    def __init__(self, ketchup_preference=None, meat_preference=None, mustard_preference = None, price_point=None):
+        self.ketchup_preference = (
+            ketchup_preference if ketchup_preference is not None
             else random.randint(1, 3)
         )
-        self.ice_preference = (
-            ice_preference if ice_preference is not None
-            else random.randint(0, 3)
+        self.meat_preference = (
+            meat_preference if meat_preference is not None
+            else random.randint(1, 3)
         )
         self.price_point = (
             price_point if price_point is not None
-            else round(random.uniform(0.25, 4.0),2)
+            else round(random.uniform(3.0, 4.0),2)
         )
-
+        self.mustard_preference = (
+            meat_preference if meat_preference is not None
+            else random.randint(1, 3)
+        )
     def get_customer_attributes(self):
         return {
-            "sweetness": self.sweetness_preference,
-            "ice": self.ice_preference,
+            "ketchup": self.ketchup_preference,
+            "meat": self.meat_preference,
+            "mustard":self.mustard_preference,
             "price": self.price_point
         }
--- startday.py
+++ startday.py
@@ -1,7 +1,12 @@
 import shop_money
+from customer_class import Customer
+import recipes
 
-def startDay():
-    global dailyEarnings
-    dailyEarnings = 60
-    shop_money.money += dailyEarnings
-    print("Daily Earnings: $" + str(dailyEarnings))
+customers = []
+attributes = customers.get_customer_attributes(self = None)
+
+def startDay(customers):
+    for customers in range(10):
+        customers.append(Customer())
+    shop_money.money += None
+    print("Daily Earnings: $" + str(None))
