114ef3c9e74589b8753247c74d11fd9579dfb7a3
DavisLBradley13 <db536877@georgetownisd.org>
2026-02-10 15:04:11 -0600

    Update startday.py

--- startday.py
+++ startday.py
@@ -1,12 +1,28 @@
-import shop_money
+from shop_money import money
 from customer_class import Customer
 import recipes
+import pricing
+import inventory
 
-customers = []
-attributes = customers.get_customer_attributes(self = None)
+buy_noBuy = None
+numOfBuy = 0
+customers = Customer()
 
 def startDay(customers):
     for customers in range(10):
         customers.append(Customer())
-    shop_money.money += None
-    print("Daily Earnings: $" + str(None))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    #this is the end
+    math1 = int(pricing.pricePer)*numOfBuy
+    print(f"Profit: {math1}")
