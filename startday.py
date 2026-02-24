59e5356e88a1cb1a6314b5da2024fe916ca1be19
ChaseMcDowell <cm545347@georgetownisd.org>
2026-02-24 10:23:28 -0600

    bug fixes
    not sure if they worked need to debug

--- startday.py
+++ startday.py
@@ -18,14 +18,14 @@ def startDay(customers_list):
     bad_meat_feedback = 0
     price_feedback = 0
     global numOfBuy
-    while inventory.inventoryVar["money"] > 0:
-        if inventory.inventoryVar["meat"] < recipes.ingredients["meat"] or inventory.inventoryVar["ketchup"] < recipes.ingredients["ketchup"] and inventory.inventoryVar["mustard"] < recipes.ingredients["mustard"] and inventory.inventoryVar["buns"] < 1 and inventory.inventoryVar["money"] == 0:
-            print("You Lose!")
-            winlose = False    
-        for _ in range(10):
+    for _ in range(10):
             customer = Customer()
             customers_list.append(customer.get_customer_attributes())
+    while True:
         for i in range(random.randint(20,75)):
+            if inventory.inventoryVar["meat"] < recipes.ingredients["meat"] or inventory.inventoryVar["mustard"] < recipes.ingredients["mustard"] or inventory.inventoryVar["ketchup"] < recipes.ingredients["ketchup"] or inventory.inventoryVar["buns"] < 1:
+                soldOut = True
+                break
             customerInLine = random.choice(customers_list)
             if recipes.ingredients["ketchup"] + recipes.ingredients["mustard"] - 1 > customerInLine["condiments"]:
                 too_condiment_feedback += 1
@@ -38,7 +38,13 @@ def startDay(customers_list):
             elif pricing.pricePer > customerInLine["price"]:
                 price_feedback +=1
             else:
+                inventory.inventoryVar["meat"] -= recipes.ingredients["meat"]
+                inventory.inventoryVar["mustard"] -= recipes.ingredients["mustard"]
+                inventory.inventoryVar["ketchup"] -= recipes.ingredients["ketchup"]
+                inventory.inventoryVar["buns"] -= 1
                 numOfBuy += 1
+        if soldOut == True:
+            print("You sold out!")
         profit = float(pricing.pricePer)*numOfBuy
         inventory.inventoryVar["money"] += profit
         print(f"Profit: {profit}")
