99a6029d80aeec741788dd4bf6fdb3046d3d2d27
DavisLBradley13 <db536877@georgetownisd.org>
2026-03-05 15:36:04 -0600

    Update backstory.py

--- backstory.py
+++ backstory.py
@@ -2,7 +2,7 @@ def story_mode():
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print("Welcome to your hotdog stand. ")
     print("Your stand is one of the most famous hotdog joints in the whole world. ")
-    print("You are located at Minute Maid Park in Houston, Tx. ")
+    print("You are located next to Minute Maid Park in Houston, Tx. ")
     print("This stadium is the home of the Houson Astros. ")
     print("Your business partners decided to quit right when the World Series were nearing and over 40,000 people are at each game. ")
     print("Your goal is to make it through the series/7 days, without going bankrupt starting with only $100.")
